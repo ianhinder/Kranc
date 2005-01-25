c79a292aeee6f3b0424feafd010fe9e29270a21a
shusa <shusa>
2005-01-25 15:40:40 +0000

    a place to collect small functions that don't fit elsewhere and would otherwise be duplicated in local code

---
+++ Tools/CodeGen/Helpers.m
@@ -0,0 +1,41 @@
+
+(* $Id$ *)
+
+(*  Copyright 2004 Sascha Husa, Ian Hinder, Christiane Lechner
+
+    This file is part of Kranc.
+
+    Kranc is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    Kranc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*)
+BeginPackage["sym`"];
+{INV, SQR, CUB, QAD, exp, pow};
+
+EndPackage[];
+
+BeginPackage["Helpers`", {"sym`"}];
+
+TensorName::usage = "get the base name of a tensor object, i.e. TensorName[g[la, lb]]";
+EnsureDirectory::usage = "create a directory if it does not already exist"
+
+Begin["`Private`"];
+
+TensorName[t_[i__]] := t 
+
+EnsureDirectory[name_] := If[FileType[name] == None, CreateDirectory[name]];
+
+End[];
+
+
+
+EndPackage[];

c795fb285d1a2c53b164a06de4b713a9fc87b69e
ianhin <ianhin>
2006-11-09 19:47:57 +0000

    Adding fmax and fmin as symbols in the sym context

--- Tools/CodeGen/CodeGen.m
+++ Tools/CodeGen/CodeGen.m
@@ -20,7 +20,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *)
 BeginPackage["sym`"];
-{INV, SQR, CUB, QAD, exp, pow};
+{INV, SQR, CUB, QAD, exp, pow, fmax, fmin};
 
 EndPackage[];
 

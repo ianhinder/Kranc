c5f19fde518926af58e9a671ab9470c6d4259988
Ian Hinder <ian.hinder@aei.mpg.de>
2010-04-03 10:58:48 +0100

    Interface.m: Remove duplicate Kranc` context on BeginPackage line

--- Tools/CodeGen/Interface.m
+++ Tools/CodeGen/Interface.m
@@ -18,7 +18,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *)
 
-BeginPackage["Interface`", {"Thorn`", "KrancGroups`", "MapLookup`", "Errors`", "Helpers`", "Kranc`", "Kranc`"}];
+BeginPackage["Interface`", {"Thorn`", "KrancGroups`", "MapLookup`", "Errors`", "Helpers`", "Kranc`"}];
 
 CreateKrancInterface;
 

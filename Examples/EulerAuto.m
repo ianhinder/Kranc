7124562a1176017be008ff060aa9aab9d45af582
Ian Hinder <ian.hinder@aei.mpg.de>
2010-10-01 22:16:20 +0200

    EulerAuto.m: Tidy up and remove unnecessary code

--- Examples/EulerAuto.m
+++ Examples/EulerAuto.m
@@ -4,42 +4,11 @@ Get["KrancThorn`"];
 SetEnhancedTimes[False];
 
 (**************************************************************************************)
-(* Derivatives *)
-(**************************************************************************************)
-
-derivatives =
-{
-  PDstandard2nd[i_] -> StandardCenteredDifferenceOperator[1,1,i],
-  PDstandard2nd[i_, i_] -> StandardCenteredDifferenceOperator[2,1,i],
-  PDstandard2nd[i_, j_] -> StandardCenteredDifferenceOperator[1,1,i] *
-    StandardCenteredDifferenceOperator[1,1,j],
-
-  PDstandard4th[i_] -> StandardCenteredDifferenceOperator[1,2,i],
-  PDstandard4th[i_, i_] -> StandardCenteredDifferenceOperator[2,2,i],
-  PDstandard4th[i_, j_] -> StandardCenteredDifferenceOperator[1,2,i] *
-    StandardCenteredDifferenceOperator[1,2,j],
-
-  PDonesided2nd[1] -> dir[1] (-shift[1]^(2 dir[1]) + 4 shift[1]^dir[1] - 3 )/(2 spacing[1]),
-  PDonesided2nd[2] -> dir[2] (-shift[2]^(2 dir[2]) + 4 shift[2]^dir[2] - 3 )/(2 spacing[2]),
-  PDonesided2nd[3] -> dir[3] (-shift[3]^(2 dir[3]) + 4 shift[3]^dir[3] - 3 )/(2 spacing[3]),
-
-  PDplus[i_] -> DPlus[i],
-  PDminus[i_] -> DMinus[i],
-  DiffPlus[i_] -> DiffPlusOp[i],
-  DiffMinus[i_] -> DiffMinusOp[i],
-  ShiftMinus[i_] -> 1/shift[i]
-};
-
-(* PD = PDstandard2nd; *)
-(* PD = PDminus; *)
-
-(**************************************************************************************)
 (* Tensors *)
 (**************************************************************************************)
 
 (* Register the tensor quantities with the TensorTools package *)
-Map[DefineTensor, {Den, S, En, rho, v, p, DenF, DenRight, DenLeft, SF, SRight, SLeft,
-  EnF, EnRight, EnLeft, vLeft, vRight, rhoLeft, rhoRight, pLeft, pRight, vRightTemp}];
+Map[DefineTensor, {Den, S, En, rho, v, p}];
 
 (**************************************************************************************)
 (* Groups *)
@@ -54,26 +23,11 @@ nonevolvedGroups = Map[CreateGroupFromTensor,
 declaredGroups = Join[evolvedGroups, nonevolvedGroups];
 declaredGroupNames = Map[First, declaredGroups];
 
-groups = Join[declaredGroups];
+groups = declaredGroups;
 
 (**************************************************************************************)
 (* Initial data *)
 (**************************************************************************************)
-
-(* initialSineCalc = *)
-(* { *)
-(*   Name -> "eulerauto_initial_sine", *)
-(*   Schedule -> {"at CCTK_INITIAL as eulerauto_initial"}, *)
-(*   ConditionalOnKeyword -> {"initial_data", "sine"}, *)
-(*   Equations -> *)
-(*   { *)
-(*     rho -> 1 + amp Sin[2 Pi x], *)
-(*     v1 -> v0, *)
-(*     v2 -> 0, *)
-(*     v3 -> 0, *)
-(*     p -> 1 (\* Rewrite this so that we get something compatible with En = 1 *\) *)
-(*   } *)
-(* }; *)
 
 initialShockCalc =
 {
@@ -108,20 +62,6 @@ initialShockCalc =
 
 *)
 
-realParameters = {sigma, v0, amp, rhoR0, rhoL0, vR0, vL0, pR0, pL0, gamma, alpha};
-
-keywordParameters = {
-  {
-    Name -> "initial_data",
-    Default -> "shock",
-    AllowedValues -> {"shock"}
-  }
-};
-
-(**************************************************************************************)
-(* Conservation calculation *)
-(**************************************************************************************)
-
 eulerCons =
 {
   Name -> "eulerauto_cons_calc",
@@ -149,6 +89,20 @@ eulerCons =
 }
 
 (**************************************************************************************)
+(* Parameters *)
+(**************************************************************************************)
+
+realParameters = {sigma, v0, amp, rhoR0, rhoL0, vR0, vL0, pR0, pL0, gamma};
+
+keywordParameters = {
+  {
+    Name -> "initial_data",
+    Default -> "shock",
+    AllowedValues -> {"shock"}
+  }
+};
+
+(**************************************************************************************)
 (* Construct the thorn *)
 (**************************************************************************************)
 
@@ -163,6 +117,5 @@ CreateKrancThornTT[groups, ".", "EulerAuto",
   Calculations -> calculations,
   ConservationCalculations -> consCalculations,
   DeclaredGroups -> declaredGroupNames,
-  PartialDerivatives -> derivatives,
   RealParameters -> realParameters,
   KeywordParameters -> keywordParameters];

295daad9bede16b5262055c48adf0480245609e8
Ian Hinder <ian.hinder@aei.mpg.de>
2012-01-24 19:19:35 -0600

    CaKernel.m: Formatting change

--- Tools/CodeGen/CaKernel.m
+++ Tools/CodeGen/CaKernel.m
@@ -18,7 +18,8 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *)
 
-BeginPackage["CaKernel`", {"Errors`", "Helpers`", "Kranc`", "CodeGenCactus`", "MapLookup`", "Calculation`", "CodeGen`"}];
+BeginPackage["CaKernel`", {"Errors`", "Helpers`", "Kranc`", "CodeGenCactus`", "MapLookup`",
+                           "Calculation`", "CodeGen`"}];
 
 CaKernelCCL;
 CaKernelCode;

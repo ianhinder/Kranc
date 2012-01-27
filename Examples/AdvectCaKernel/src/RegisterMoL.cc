d3c1d765b67c103f714a22d6d62d0465f1639c09
Ian Hinder <ian.hinder@aei.mpg.de>
2012-01-27 15:29:53 -0600

    Generate AdvectCaKernel

---
+++ Examples/AdvectCaKernel/src/RegisterMoL.cc
@@ -0,0 +1,18 @@
+/*  File produced by Kranc */
+
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+
+extern "C" void AdvectCaKernel_RegisterVars(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+  
+  CCTK_INT ierr = 0;
+  
+  /* Register all the evolved grid functions with MoL */
+  
+  /* Register all the evolved Array functions with MoL */
+  return;
+}

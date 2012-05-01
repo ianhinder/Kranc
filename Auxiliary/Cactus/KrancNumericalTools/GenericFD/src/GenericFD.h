90900d18be8a0b77eb736e622195eecaeaf93bbd
Ian Hinder <ian.hinder@aei.mpg.de>
2012-05-01 21:58:57 +0200

    GenericFD: Correct implementation of Sign
    Mathematica defines Sign(x) = Which[x>0, 1, x<0, -1, x==0, 0] whereas
    the current implementation in terms of copysign gives Sign(0) != 0.
    This causes problems when Sign is used to choose the finite
    differencing operator in boundary conditions.  This commit restores
    the old definition in terms of the sgn function which we define now as
    static inline in GenericFD.h.

--- Auxiliary/Cactus/KrancNumericalTools/GenericFD/src/GenericFD.h
+++ Auxiliary/Cactus/KrancNumericalTools/GenericFD/src/GenericFD.h
@@ -54,7 +54,11 @@ extern "C" {
 #define KRANC_GFOFFSET3D(var,i,j,k)                                     \
   (*(CCTK_REAL const*)&((char const*)(var))[cdi*(i)+cdj*(j)+cdk*(k)])
 
-int sgn(CCTK_REAL x);
+/* Implement the signum function, used for Mathematica's Sign function */
+static inline CCTK_REAL sgn(CCTK_REAL x)
+{
+ return x==(CCTK_REAL)0.0 ? (CCTK_REAL)0.0 : copysign((CCTK_REAL)1.0, x);
+}
 
 int GenericFD_GetBoundaryWidth(cGH const * restrict const cctkGH);
 

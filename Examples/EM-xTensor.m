b57def09de36c82d034fe8cf76f1d994c6b775c6
Barry Wardell <barry.wardell@gmail.com>
2014-03-05 18:59:38 -0500

    Remove unneded code from xTensor example

--- Examples/EM-xTensor.m
+++ Examples/EM-xTensor.m
@@ -19,9 +19,6 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *)
 
-$KrancTensorPackage="xTensorKranc`";
-Get["KrancTensor`"];
-
 PrependTo[$Path, "../../EinsteinExact/m"];
 
 Check[Get["Metrics`LoadMetric`"],

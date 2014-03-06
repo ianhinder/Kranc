bc4e9108121c8f0a37839675696e754f0c695be5
Barry Wardell <barry.wardell@gmail.com>
2014-03-06 13:23:51 -0500

    Regenerate EM example with master branch commit eca6928f0542a28dcfad3dbc2bb3fec69a128cd6

--- Examples/EM/src/RegisterSymmetries.cc
+++ Examples/EM/src/RegisterSymmetries.cc
@@ -19,21 +19,6 @@ extern "C" void EM_RegisterSymmetries(CCTK_ARGUMENTS)
   sym[0] = -1;
   sym[1] = 1;
   sym[2] = 1;
-  SetCartSymVN(cctkGH, sym, "EM::B1");
-  
-  sym[0] = 1;
-  sym[1] = -1;
-  sym[2] = 1;
-  SetCartSymVN(cctkGH, sym, "EM::B2");
-  
-  sym[0] = 1;
-  sym[1] = 1;
-  sym[2] = -1;
-  SetCartSymVN(cctkGH, sym, "EM::B3");
-  
-  sym[0] = -1;
-  sym[1] = 1;
-  sym[2] = 1;
   SetCartSymVN(cctkGH, sym, "EM::El1");
   
   sym[0] = 1;
@@ -45,6 +30,21 @@ extern "C" void EM_RegisterSymmetries(CCTK_ARGUMENTS)
   sym[1] = 1;
   sym[2] = -1;
   SetCartSymVN(cctkGH, sym, "EM::El3");
+  
+  sym[0] = -1;
+  sym[1] = 1;
+  sym[2] = 1;
+  SetCartSymVN(cctkGH, sym, "EM::B1");
+  
+  sym[0] = 1;
+  sym[1] = -1;
+  sym[2] = 1;
+  SetCartSymVN(cctkGH, sym, "EM::B2");
+  
+  sym[0] = 1;
+  sym[1] = 1;
+  sym[2] = -1;
+  SetCartSymVN(cctkGH, sym, "EM::B3");
   
   sym[0] = 1;
   sym[1] = 1;

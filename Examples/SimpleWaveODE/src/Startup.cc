2ec73695b72139fb31a78bdecb9318ca64983b13
Barry Wardell <barry.wardell@gmail.com>
2014-03-07 17:21:12 -0500

    Regenerate SimpleWaveODE example with master branch commit eca6928f0542a28dcfad3dbc2bb3fec69a128cd6

--- Examples/SimpleWaveODE/src/Startup.cc
+++ Examples/SimpleWaveODE/src/Startup.cc
@@ -4,7 +4,7 @@
 
 extern "C" int SimpleWaveODE_Startup(void)
 {
-  const char * banner CCTK_ATTRIBUTE_UNUSED  = "SimpleWaveODE";
+  const char* banner CCTK_ATTRIBUTE_UNUSED = "SimpleWaveODE";
   CCTK_RegisterBanner(banner);
   return 0;
 }

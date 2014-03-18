d2a2e5c211e95ca1c29487bff33547bbff986bd9
Ian Hinder <ian.hinder@aei.mpg.de>
2014-03-18 11:51:57 +0100

    RunTests.m: Track global variables and handle case of End[] not being called after a test failure

--- Tests/RunTests.m
+++ Tests/RunTests.m
@@ -31,10 +31,25 @@ If[Length[args] > 0,
   tests = FileBaseName/@Select[args, StringMatchQ[#, "*.mt"] &],
   tests = alltests];
 
-results = (Print["\n"]; TestRun[#<>".mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> #]) & /@ tests;
+runTest[filename_String] :=
+  Module[{globalBefore = Select[Names["Global`*"],StringFreeQ[#,"$"]&], globalAfter,
+    globalNew, result, contextBefore, contextAfter},
+    Print["\n"];
+    contextBefore = $Context;
+    result = TestRun[filename<>".mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> filename];
+    contextAfter = $Context;
+    (* Test failures cause the final End[] in a test file to not be evaluated *)
+    If[contextAfter =!= contextBefore, $Context = contextBefore];
+    globalAfter = Select[Names["Global`*"],StringFreeQ[#,"$"]&];
+    globalNew = Complement[globalAfter, globalBefore];
+    (* If[globalNew =!= {}, Print[]; Print[]; Print[filename, ".mt: Symbols leaked into Global` context: ", globalNew//InputForm]]; *)
+    result];
+
+results = runTest /@ tests;
 Print[];
 Print[];
-MapThread[Print[If[#2,"P","F"]," ",#1] &, {tests, results}];
+Print["Test files run:"];
+MapThread[Print[If[#2," ","F"]," ",#1] &, {tests, results}];
 Print[];
 
 If[And@@results, Print["All tests passed"],

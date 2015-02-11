b697a036d2586046012e65448032a05b8ab6ad19
Steven R. Brandt <sbrandt@cct.lsu.edu>
2015-02-11 15:48:34 -0600

    Got basic derivatives, etc. working.

--- Tools/CodeGen/KrancScript.m
+++ Tools/CodeGen/KrancScript.m
@@ -67,7 +67,7 @@ process[thorn:"thorn"[content___]] :=
               "parameters"[__], parameters = Join[parameters,List@@Map[process,el]],
               "variables"[__], variables = Join[variables,List@@Map[process,el]],
               "temporaries"[__], temporaries = Join[temporaries,List@@Map[process,el]],
-              "derivatives"[__], derivatives = Join[derivatives,process[el]],
+              "derivatives"[__], (* derivatives = *) Join[derivatives,process[el]],
               "option"[__], options = Join[options, process[el]],
               _, ThrowError["Unrecognised element '"<>Head[el]<>"' in thorn"]],
        {el, {content}}];
@@ -137,14 +137,25 @@ process["tensor"["name"[k_],inds_]] :=
 
 fprint[x_] := (Print[x//InputForm]; x);
 
+(* Simple derivative rules *)
+PDiv[n1_?NumberQ,inds_] := 0;
+PDiv[n1_?NumberQ x2,inds_] := n1 PDiv[x2,inds];
+PDiv[x1_+x2_,inds_] := PDiv[x1,inds]+PDiv[x2,inds];
+PDiv[x1_ x2_,inds_] := PDiv[x1,inds] x2 + PDiv[x2,inds] x1;
+PDiv[x1_^n2_?NumberQ,inds_] := n2 x1^(n2-1) PDiv[x1,inds];
+PDiv[x1_,inds_] := PD[x1,inds]
 
+TensorStrQ["tensor"[__]] := True;
+TensorStrQ[__] := False;
 
-process["dtensor"["dname"[dname_],inds_,tensor_]] := ToExpression[dname][process[tensor],Sequence@@process[inds]];
+process["dtensor"["dname"[dname_],inds_,"tensor"[tensor__]]] := ToExpression[dname][process["tensor"[tensor]],Sequence@@process[inds]];
 
-process["dtensor"["dname"["D"],inds_,tensor_]] := PD[process[tensor],Sequence@@process[inds]];
+process["dtensor"["dname"["D"],inds_,"tensor"[tensor__]]] := (Print[">>tensor[",tensor,"] inds=[",inds,"] ",Sequence@@process[inds]];PD[process["tensor"[tensor]],Sequence@@process[inds]]);
+process["dtensor"["dname"["D"],inds_,"expr"[tensor__]]] := (Print[">>expr[",process[tensor],"] pdiv=[",PDiv[process[tensor],lx],"] inds=[",inds,"] ",Sequence@@process[inds]];
+  PDiv[process["expr"[tensor]],Sequence@@process[inds]]);
 
-process["dtensor"["dname"["D"], "indices"["lower_index"["index_symbol"["t"]]],tensor_]] :=
-  dot[process[tensor]];
+process["dtensor"["dname"["D"], "indices"["lower_index"["index_symbol"["t"]]],"tensor"[tensor__]]] :=
+  dot[process["tensor"[tensor]]];
 
 process["indices"[inds___]] := Map[process, {inds}];
 
@@ -159,7 +170,8 @@ process["func"["name"[name_],exprs__]] :=
     fns = {"sin" -> Sin, "cos" -> Cos, "if" -> IfThen, "max" -> Max,
            "sqrt" -> Sqrt, "exp" -> Exp};
     If[MemberQ[First/@fns,name], (name/.fns)@@Map[process,{exprs}],
-       ThrowError["Unrecognised function: ", name]]];
+       (* If it's not a function call, it's an explicit multiply *)
+       process["mul"["mexpr"["mul"["pow"["value"["tensor"["name"[name],"indices"[]]]]]],"mulop"["*"],"mexpr"[exprs]]]]];
 
 process["mexpr"[mul_]] := process[mul];
 process["mexpr"[]] := 0;
@@ -174,6 +186,7 @@ process["expr"["neg"[_],m:"mexpr"[___]]] := -process[m];
 process["mul"[pow_]] := process[pow];
 process["mul"[]] := 1;
 process["mul"[cs___, a_, "mulop"["*"], b_]] := process["mul"[cs,a]] * process[b];
+process["mul"[cs___, a_, "mulimp"[___], b_]] := process["mul"[cs,a]] * process[b];
 process["mul"[cs___, a_, "mulop"["/"], b_]] := process["mul"[cs,a]] / process[b];
 
 process["pow"[a_,b_]] := process[a]^process[b];
@@ -196,20 +209,30 @@ process["number"[num_],"rightenc"[sym_]] := num <> sym;
 process["infinity"[_]] := "*";
 
 processRange[value_,minOrMax_,paramName_] :=
-  If[StringQ[value] && value == "*",value,
+  If[StringQ[value] && value === "*",value,
     Module[{tmp},
       tmp = N[value];
       If[NumberQ[tmp],1,ThrowError[minOrMax<>" value for parameter "<>paramName<>" is not a number"]];
       Return[tmp]]]
 
-process["parameter"["name"[nm_],"desc"[desc_],"expr"[def_],"parlo"[le__],"parhi"[re__]]] := 
+process["parameter"["name"[nm_],"type"[desc_],"quote"[def_]]] :=
+Module[{dtmp,lotmp,hitmp},
+  dtmp = 1.0;
+  lotmp = "*";
+  hitmp = "*";
+  {Name -> ToExpression[nm],
+   Description -> StringTake[desc,{2,StringLength[desc]-1}],
+   AllowedValues -> {{Value->ToString[lotmp] <> ":" <> ToString[hitmp]}},
+   Default -> dtmp}]
+
+process["parameter"["name"[nm_],"type"["real"],"quote"[desc_],"expr"[def_],"parlo"[le__],"parhi"[re__]]] := 
 Module[{dtmp,lotmp,hitmp},
   dtmp = N[process[def]];
   lotmp = processRange[process[le],"Minimum",nm];
   hitmp = processRange[process[re],"Maximum",nm];
   If[NumberQ[dtmp],1,ThrowError["Default value for parameter "<>nm<>" is not a number"]];
   {Name -> ToExpression[nm],
-   Description -> desc,
+   Description -> StringTake[desc,{2,StringLength[desc]-1}],
    AllowedValues -> {{Value->ToString[lotmp] <> ":" <> ToString[hitmp]}},
    Default -> dtmp}]
 
@@ -231,6 +254,7 @@ process[d:"deqn"[___]] :=
     Print["(deqn) No handler for ", d];
     ThrowError["Failed to parse script"]];
 
+process["deqn"["doper"["dname"[dn_],"indices"[ind__]],"expr"[ex_]]] := "";
 process["deqn"[("dtensor"["dname"[dname_],
                           "tensor"["name"[tName_],
                                    "indices"[tinds___]]]),

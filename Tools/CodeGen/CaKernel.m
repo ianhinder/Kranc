ianhinder/Kranc

commit 021e0009cf00ada6a8b670f4d22daccce99822dc

Fix segmentation fault in host code.
 - Declare local (grid point) variables as references, to fix
     error with I3D.

AssignVariableFromExpression: Option to declare as reference.
DefineReference: New.
assignLocalFunctions: Declare simple grid variables as references.

Tools/CodeGen/CodeGenCalculation.m
@@ -719,7 +719,9 @@ DefFn[assignLocalFunctions[gs:{_Symbol...}, useVectors:Boolean, useJacobian:Bool
 
     code = {"\n",
       (* Simple grid variables *)
-      Map[AssignVariableFromExpression[localName[#],GFLocal[#],True,useVectors,True] &, simpleVars],
+      Map[AssignVariableFromExpression
+            [localName[#],GFLocal[#],True,useVectors,True,Reference->True] &,
+          simpleVars],
       {"\n",
         (* Conditional grid variables *)
         NewlineSeparated@

Tools/CodeGen/CodeGenC.m
@@ -45,6 +45,8 @@ DeclarePointers::usage = "DeclarePointers[names, type] returns a block of code "
   " of strings and 'type' should be a string string.";
 DefineVariable::usage = "DefineVariable[name, type, value] returns a block of " <>
   "code that declares and initialises a variable 'name' of type 'type' to value 'value'.";
+DefineReference::usage = "DefineReference[name, type, value] returns a block of " <>
+  "code that declares a reference named 'name' to lvalue of expression 'value' of type 'type'.";
 DefineConstant::usage = "DefineConstant[name, type, value] returns a block of " <>
   "code that declares and initialises a constant 'name' of type 'type' to value 'value'.";
 AssignVariable::usage = "AssignVariable[dest_, src_] returns a block of code " <>
@@ -94,6 +96,10 @@ DefFn[
 DefFn[
   DefineVariable[name:(_String|_Symbol), type_String, value:CodeGenBlock] :=
   {type, " ", name, " CCTK_ATTRIBUTE_UNUSED", " = ", value, ";\n"}];
+
+DefFn[
+  DefineReference[name:(_String|_Symbol), type_String, value:CodeGenBlock] :=
+  {type, "& ", name, " CCTK_ATTRIBUTE_UNUSED", " = ", value, ";\n"}];
 
 DefFn[
   DefineConstant[name:(_String|_Symbol), type_String, value:CodeGenBlock] :=

Tools/CodeGen/CodeGenKranc.m
@@ -287,7 +287,8 @@ DefFn[PostProcessExpression[t_TargetC, expr_] :=
     expr2]];
 
 (* Return a CodeGen block which assigns dest by evaluating expr *)
-Options[AssignVariableFromExpression] = {"Const" -> False, "Type" -> Automatic};
+Options[AssignVariableFromExpression] =
+   {"Const" -> False, "Type" -> Automatic, "Reference" -> False};
 DefFn[AssignVariableFromExpression[dest_, expr_, declare_, vectorise_, noSimplify:Boolean : False,
                              OptionsPattern[]] :=
   Module[{type, exprCode, code},
@@ -297,8 +298,14 @@ DefFn[AssignVariableFromExpression[dest_, expr_, declare_, vectorise_, noSimplif
       OptionValue[Type]];
     exprCode = GenerateCodeFromExpression[expr, vectorise, noSimplify];
     CountOperations[expr];
+    If[ OptionValue[Const] && OptionValue[Reference],
+          ThrowError
+            ["Internal Error, cannot declare const reference variables."] ];
     code = If[declare,
-              If[OptionValue[Const],DefineConstant,DefineVariable][dest, type, exprCode],
+              If[OptionValue[Const],
+                   DefineConstant,
+                   If[OptionValue[Reference],DefineReference,DefineVariable]]
+              [dest, type, exprCode],
               AssignVariable[dest, exprCode]];
     code = LineBreak[FlattenBlock[code], 70] <> "\n";
     {code}]];

commit 6c89ca9e3832eff7720a4592c929e7bbc77a3524

Fix incorrect execution in statically compiled CaKernel code.
 - This is a regression introduced in 021e000 (2 Sep 2017).
 - Avoid using reference types for GF locals in CaKernel ".code" files.

Tools/CodeGen/CaKernel.m
@@ -220,7 +220,7 @@ DefFn[CaKernelCode[calc_List,opts___] :=
      "\n",
 
     "\n", CreateCalculationFunction[calc2, LoopName -> kernel<>"_Computations",
-                                    opts]}]]];
+                                    opts, CodeTarget -> Device]}]]];
 
 DefFn[
   splitHostCaKernel[calc_List] :=

Tools/CodeGen/CodeGenCalculation.m
@@ -156,7 +156,7 @@ DefFn[CreateSetterSource[calcs_, debug_, include_, thornName_,
                       InitFDVariables -> InitialiseFDVariables[OptionValue[UseVectors]],
                       MacroPointer -> True}];
 
-   CreateCalculationFunction[calc, opts]}]]}]};
+   CreateCalculationFunction[calc, opts, CodeTarget -> Host]}]]}]};
    mapRefSet[calc,ChemoraContents,chemoraContentsSave];
    rv ]]];
 
@@ -487,7 +487,8 @@ chemoraCSimpleExpression[c_, anything_]:=
 chemoraCExpression[c_,eqs_] :=
    chemoraCExpressionEQ[c,#] & /@ ( eqs //. chemoraExpandRules );
 
-Options[CreateCalculationFunction] = Join[ThornOptions,{Debug -> False}];
+Options[CreateCalculationFunction] =
+   Join[ThornOptions,CodeGenOptions, {Debug -> False}];
 
 DefFn[
   CreateCalculationFunction[calcp_, opts:OptionsPattern[]] :=
@@ -708,8 +709,10 @@ DefFn[
        ""]
   }]];
 
+Options[assignLocalFunctions] = Join[ThornOptions,CodeGenOptions];
+
 (* Create definitions for the local copies of gridfunctions or arrays *)
-DefFn[assignLocalFunctions[gs:{_Symbol...}, useVectors:Boolean, useJacobian:Boolean, nameFunc_] :=
+DefFn[assignLocalFunctions[gs:{_Symbol...}, useVectors:Boolean, useJacobian:Boolean, nameFunc_,opts:OptionsPattern[]] :=
   Module[{conds, varPatterns, varsInConds, simpleVars, code},
 
     (* Conditional access to grid variables *)
@@ -727,7 +730,8 @@ DefFn[assignLocalFunctions[gs:{_Symbol...}, useVectors:Boolean, useJacobian:Bool
     code = {"\n",
       (* Simple grid variables *)
       Map[AssignVariableFromExpression
-            [localName[#],GFLocal[#],True,useVectors,True,Reference->True] &,
+            [localName[#],GFLocal[#],True,useVectors,True,
+             Reference -> OptionValue[CodeTarget] === Host] &,
           simpleVars],
       {"\n",
         (* Conditional grid variables *)
@@ -743,7 +747,7 @@ DefFn[assignLocalFunctions[gs:{_Symbol...}, useVectors:Boolean, useJacobian:Bool
           {Map[#[[2]]&, conds], varsInConds, Map[#[[3]]&, conds]}]}};
   code]];
 
-Options[equationLoop] = ThornOptions;
+Options[equationLoop] = Join[ThornOptions, CodeGenOptions];
 
 HasBadDeriv[eqn_,shorts_] := 
     eqn /. Global`PDstandard[arg_,_,_] :>
@@ -766,8 +770,7 @@ gfMakeAssign[gf_] :=
 
 DefFn[
   equationLoop[eqs_, cleancalc_, gfs_, shorts_, incs_, groups_, odeGroups_, pddefs_,
-             where_, addToStencilWidth_,
-             opts:OptionsPattern[]] :=
+             where_, addToStencilWidth_, opts:OptionsPattern[]] :=
   Module[{rhss, lhss, gfsInRHS, gfsInLHS, gfsOnlyInRHS, localGFs,
           localMap, eqs2, derivSwitch, code, functionName, calcCode,
           gfsInBoth, gfsDifferentiated,
@@ -906,7 +909,7 @@ DefFn[
     calcCodeArrays = Riffle[generateEquationCode /@ groupedIfsArrays, "\n"];
     InfoMessage[InfoFull, "Finished generating equation code"];
 
-    assignLocalGridFunctions[gs_, useVectors_, useJacobian_] := assignLocalFunctions[gs, useVectors, useJacobian, gridName];
+    assignLocalGridFunctions[gs_, useVectors_, useJacobian_] := assignLocalFunctions[gs, useVectors, useJacobian, gridName, opts];
     assignLocalArrayFunctions[gs_] := assignLocalFunctions[gs, False, False, ArrayName];
 
     (* separate grid and array variables *)

Tools/CodeGen/Kranc.m
@@ -129,6 +129,8 @@ ThornOptions =
   MergeFiles -> None,
   CountOperations -> False};
 
+CodeGenOptions = { CodeTarget -> Device };
+
 ExecuteOn;
 Device;
 Host;